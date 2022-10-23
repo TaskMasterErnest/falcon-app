3ba7cb844b9fcac381910fb0f84029007004dc94
TaskMasterErnest <ernestklu08@gmail.com>
2022-10-23 13:34:34 +0000

    added workflows, final commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,7 +17,7 @@ def test_list_images(client):
     doc = {
         'images': [
             {
-                'href': '/image/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
+                'href': '/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png'
             }
         ]
     }
